3d869c390457e1d43068df4f71848745e7b0a4d2
Ojo Emmanuel T <chincoekun2002@gmail.com>
2024-10-24 00:33:55 -0700

    change-third

--- components/Stat.tsx
+++ components/Stat.tsx
@@ -56,7 +56,7 @@ export default function StatsSection() {
             <CardTitle>Monthly Profit Trend</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="h-[300px]">
+            <div className="h-[300px] bg-white">
               <ResponsiveContainer width="100%" height="100%">
                 <BarChart data={data}>
                   <CartesianGrid strokeDasharray="3 3" />
